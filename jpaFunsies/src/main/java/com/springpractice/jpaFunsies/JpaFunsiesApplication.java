ce405b0cf62f83ab11b512724c4d34f71c1181ee
Dusty Williams <dusty.wil@gmail.com>
2020-04-17 16:10:39 -0400

    init commit - dmw

---
+++ jpaFunsies/src/main/java/com/springpractice/jpaFunsies/JpaFunsiesApplication.java
@@ -0,0 +1,13 @@
+package com.springpractice.jpaFunsies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFunsiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaFunsiesApplication.class, args);
+	}
+
+}
